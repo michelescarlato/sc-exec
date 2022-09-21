60e7bc6b42d6aa1e5430089f5978ff3ea61e5f99
Michele Scarlato <michele.scarlato@gmail.com>
2022-09-21 07:59:25 +0200

    Deployment stores contract address to file. Private key declared only in main.

--- contracts/Deploy.js
+++ contracts/Deploy.js
@@ -4,73 +4,56 @@ const web3 = new Web3(host);
 const path = require('path');
 const fs = require('fs-extra');
 
+const fsW = require('fs')
 
 
 const contractJsonPath = path.resolve(__dirname, '../','contracts','SimpleStorage.json');
-console.log('contract :',contractJsonPath );
+//console.log('contract :',contractJsonPath );
 const contractJson = JSON.parse(fs.readFileSync(contractJsonPath));
-console.log('contenuto Contract: ',contractJson);
+//console.log('contenuto Contract: ',contractJson);
 const contractAbi = contractJson.abi;
-console.log('Abi contenuto: ',contractAbi);
+//console.log('Abi contenuto: ',contractAbi);
 const contractBytecode = contractJson.bytecode;
-console.log('BIN contenuto: ',contractBytecode);
+//console.log('BIN contenuto: ',contractBytecode);
 
 
+async function createContract(host, privateKey){
+  //const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  //console.log('account: ',account);
+  const contractConstructorInit= web3.eth.abi.encodeParameter('uint256', '47').slice('2');
+  //console.log('init: ',contractConstructorInit);
+  const tx = {
+    chainId: 1337,
+    nonce: await web3.eth.getTransactionCount(account.address),
+    from: account.address,
+    to: null,
+    value: "0x00",
+    data: '0x'+contractBytecode+contractConstructorInit,
+    gasPrice: "0x0",     //ETH per unit of gas
+    gas: "0x2CA51"
+  };
 
-
-async function createContract(host){
-
-const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
-const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-console.log('account: ',account);
-
-const contractConstructorInit= web3.eth.abi.encodeParameter('uint256', '47').slice('2');
-console.log('init: ',contractConstructorInit);
-
-const txn = {
-  chainId: 1337,
-  nonce: await web3.eth.getTransactionCount(account.address),
-  from: account.address,
-  to: null,            
-  value: "0x00",
-  data: '0x'+contractBytecode+contractConstructorInit,
-  gasPrice: "0x0",     //ETH per unit of gas
-  gas: "0x2CA51" 
+  const signedTx = await web3.eth.accounts.signTransaction(tx, account.privateKey);
+  const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  return txReceipt;
 };
 
-
-const signedTx = await web3.eth.accounts.signTransaction(txn, account.privateKey);
-
-const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-
-return txReceipt;
-
-
-};
-
-
-
-
-
-
-
-
-
-
+// NO private key needed
 async function getValueAtAddress(host, deployedContractAbi, deployedContractAddress){
   const web3 = new Web3(host);
-//const web3 = new Web3(provider);
-const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
+  //const web3 = new Web3(provider);
+  const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
   const res = await contractInstance.methods.get().call();
-  console.log("Obtained value at deployed contract is: "+ res);
+  //console.log("Obtained value at deployed contract is: "+ res);
   return res
 }
 
-
-async function setValueAtAddress(host, accountAddress, value, deployedContractAbi, deployedContractAddress){
+// private key needed
+async function setValueAtAddress(host, accountAddress, value, deployedContractAbi, deployedContractAddress, privateKey){
   const web3 = new Web3(host);
-const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
-const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  //const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
   const contract = new web3.eth.Contract(deployedContractAbi);
   const functionAbi = contract._jsonInterface.find(e => {
     return e.name === "set";
@@ -84,18 +67,16 @@ const account = web3.eth.accounts.privateKeyToAccount(privateKey);
     gas: "0x2CA51"  //max number of gas units the tx is allowed to use
   };
   const signedTx = await web3.eth.accounts.signTransaction(functionParams, account.privateKey);
-  console.log("sending the txn")
+  //console.log("sending the txn")
   const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  console.log("tx transactionHash: " + txReceipt.transactionHash);
-  console.log("tx contractAddress: " + txReceipt.contractAddress);
+  //console.log("tx transactionHash: " + txReceipt.transactionHash);
+  //console.log("tx contractAddress: " + txReceipt.contractAddress);
   return txReceipt
 }
 
-
-
 async function getAllPastEvents(host, deployedContractAbi, deployedContractAddress){
   const web3 = new Web3(host);
-const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
+  const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
   const res = await contractInstance.getPastEvents("allEvents", {
     fromBlock: 0,
     toBlock: 'latest'
@@ -103,29 +84,33 @@ const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedCont
   const amounts = res.map(x => {
     return x.returnValues._amount
   });
-  console.log("Obtained all value events from deployed contract : [" + amounts + "]");
+  //console.log("Obtained all value events from deployed contract : [" + amounts + "]");
   return res
 }
 
 
 
 async function main(){
-  createContract(host)
- .then(async function(txReceipt){
-const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
-const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-  console.log('contract deployed at address: ' +txReceipt.contractAddress);
-  console.log("Use the smart contracts 'get' function to read the contract's constructor initialized value .. " )
-  await getValueAtAddress(host, contractAbi, txReceipt.contractAddress);
-  console.log("Use the smart contracts 'set' function to update that value to 123 .. " );
-  await setValueAtAddress(host, account.address,'32', contractAbi, txReceipt.contractAddress );
-  console.log("Verify the updated value that was set .. " )
-  await getValueAtAddress(host, contractAbi, txReceipt.contractAddress);
-  await getAllPastEvents(host, contractAbi, txReceipt.contractAddress);
+  const privateKey = "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
+  createContract(host,privateKey)
+  .then(async function(txReceipt){
+   //console.log(privateKey)
+   const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+   console.log('contract deployed at address: ' +txReceipt.contractAddress);
+   //console.log("Use the smart contracts 'get' function to read the contract's constructor initialized value .. " )
+   await getValueAtAddress(host, contractAbi, txReceipt.contractAddress);
+   //console.log("Use the smart contracts 'set' function to update that value to 123 .. " );
+   await setValueAtAddress(host, account.address,'32', contractAbi, txReceipt.contractAddress, privateKey );
+   //console.log("Verify the updated value that was set .. " )
+   await getValueAtAddress(host, contractAbi, txReceipt.contractAddress);
+   await getAllPastEvents(host, contractAbi, txReceipt.contractAddress);
+   fsW.writeFile('ContractAddress.txt', txReceipt.contractAddress, (err) => {
 
-
-})
-.catch(console.error);
+       // In case of a error throw err.
+       if (err) throw err;
+     })
+   })
+  .catch(console.error);
 }
 
 main()
